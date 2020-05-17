5fa430836c0ecfd04004d41bffd2092c63ab714f
bilal khan <billalkhn98@gmail.com>
2020-05-17 17:00:48 +0500

    Crud Complete
    Delete operation .
    Datatable update/new.
    toaster message
    javascript .

--- BookListRazor/BookListRazor/Controllers/BookController.cs
+++ BookListRazor/BookListRazor/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Controllers
 {
@@ -20,9 +21,27 @@ namespace BookListRazor.Controllers
 
         }
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() });
+            return Json(new { data = await _db.Book.ToListAsync() });
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookfromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookfromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting " });
+            }
+
+            _db.Book.Remove(bookfromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successfull" });
+
+
+        }
+
+
     }
 }
